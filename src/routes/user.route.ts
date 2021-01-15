1490df5ec754908cb4aab29fa928b0a00a35e6cc
haerulmutakin <haerul.mutakin@uii.ac.id>
2021-01-15 22:26:43 +0700

    add service for delete user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,9 +3,11 @@ import express from 'express';
 const userRouter = express.Router();
 import {getUser, getUserById} from './../controllers/user/get-user.controller';
 import postUser from './../controllers/user/post-user.controller';
+import deleteUser from '../controllers/user/delete-user.controller';
 
 userRouter.get('/', getUser);
 userRouter.get('/:id', getUserById);
 userRouter.post('/', postUser);
+userRouter.delete('/:id', deleteUser)
 
 export default userRouter;
